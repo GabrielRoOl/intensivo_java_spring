4f6757b1192f01744e0ef88dae1e92ebda0bba05
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2024-11-12 09:23:34 -0300

    Project created

---
+++ src/test/java/br/com/devsuperior/dslist/IntensivoJavaScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivoJavaScriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
